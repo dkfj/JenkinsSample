56778b809e1e64a58e257d11acf0c015e89ab4c4
佐々木拓郎 <t3-sasaki@intanettoeigyousuishinshitsu-no-MacBook-Air.local>
2013-03-05 03:07:16 +0900

    add firefox test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -6,6 +6,16 @@ describe "Articles" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get articles_path
       response.status.should be(200)
+      driver = Selenium::WebDriver.for :remote, :url => "http://10.0.0.169:4444/wd/hub", :desired_capabilities => :firefox
+      driver.get "http://www.yahoo.co.jp"
+
+      wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+      begin
+        element = wait.until { driver.find_element(:id => "some-dynamic-element") }
+      ensure
+        driver.quit
+      end
+
     end
   end
 end
