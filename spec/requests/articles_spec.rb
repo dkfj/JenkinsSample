5c6c1a516ff2c02c07b848e87e99bf86a3f88f7f
佐々木拓郎 <t3-sasaki@intanettoeigyousuishinshitsu-no-MacBook-Air.local>
2013-04-26 08:22:36 +0900

    comment out selenium spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -5,10 +5,10 @@ describe "Articles" do
   describe "GET /articles" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get articles_path
-      response.status.should be(200)
-      driver = Selenium::WebDriver.for :remote, :url => "http://10.0.0.169:4444/wd/hub", :desired_capabilities => :firefox
-      driver.get "http://www.yahoo.co.jp"
+      ##get articles_path
+      ##response.status.should be(200)
+      ##driver = Selenium::WebDriver.for :remote, :url => "http://10.0.0.169:4444/wd/hub", :desired_capabilities => :firefox
+      ##driver.get "http://www.yahoo.co.jp"
 
       #wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
       #begin
@@ -17,7 +17,7 @@ describe "Articles" do
       #  driver.quit
       #end
       sleep 2
-      driver.quit
+      #driver.quit
 
     end
   end
